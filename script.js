91bc063f849fe9b0607d63379ff99a43d1cc0c72
Matthew Song <93973501+matthewsongster@users.noreply.github.com>
2025-11-08 06:22:56 -0500

    js stuff

---
+++ script.js
@@ -0,0 +1,10 @@
+const firstName = document.querySelector("#first_name")
+const lastName = document.querySelector("#last_name")
+const email = document.querySelector("#email")
+const phone = document.querySelector("#phone")
+const password = document.querySelector("#password")
+const confirm = document.querySelector("#confirm")
+
+firstName.addEventListener("blur", () => {
+    firstName.value 
+})
